1d182d9baf7ca79618bb45a7747201cb382b8cdd
stanley jony <1jony2jony@gmail.com>
2023-02-09 20:05:36 -0300

    Parametrizando front - iniciando pagina master

--- parameterized_front/behavior/resources/views/front/home.blade.php
+++ parameterized_front/behavior/resources/views/front/home.blade.php
@@ -1,20 +1,36 @@
-Meu nome é: {{ $user->name }}
-<br>
-@if (empty($user->email))
-    [IF] The user não informou e-mail
-@elseif ($user)
-    [ELSEIF] Existe um Objeto User
-@else
-    [ELSE] There is no user object
-@endif
-<br>
-@foreach ($courses as $course)
-    <p>{{ $course['name'] }}</p>
+@extends('front.master.master')
 
-    @php
-        echo '<pre>';
-            var_dump($loop);
-        echo '</pre>';
-    @endphp
+@section('title', 'Seja bem-vindo')
 
-@endforeach
+@section('content')
+
+    <div class="container">
+        <div class="row py-4">
+
+            <div class="col-12">
+                Meu nome é: {{ $user->name }}
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                @if (empty($user->email))
+                    [IF] The user não informou e-mail
+                @elseif ($user)
+                    [ELSEIF] Existe um Objeto User
+                @else
+                    [ELSE] There is no user object
+                @endif
+                <br>
+                @foreach ($courses as $course)
+                    <p>{{ $course['name'] }}</p>
+                @endforeach
+            </div>
+            <div class="col-6">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, est! Maiores nesciunt ullam ea id ad ipsa commodi voluptas, totam sequi ab, laboriosam natus quis repellat itaque, consequatur dolore vero.</p>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ parameterized_front/behavior/resources/views/front/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="mt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p class="text-center">Todos os Direitos Reservados Laravel</p>
+                <p class="text-center"><small>{{ date('Y') }} | Laravelis</small></p>
+            </div>
+        </div>
+    </div>
+</footer>
