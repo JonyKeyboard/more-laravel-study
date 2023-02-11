0818bc0b5f11cc51622715ed79aee03add6fd65b
stanley jony <1jony2jony@gmail.com>
2023-02-11 20:29:53 -0300

    Parametrizando Front - Entendendo minificação e versionamento

--- parameterized_front/behavior/resources/views/front/includes/header.blade.php
+++ parameterized_front/behavior/resources/views/front/includes/header.blade.php
@@ -1,26 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <div class="row">
-            <a class="navbar-brand" href="#">Laravel</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Curso</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Escola</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Laravel 8</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Curso</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Escola</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contato</a>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
