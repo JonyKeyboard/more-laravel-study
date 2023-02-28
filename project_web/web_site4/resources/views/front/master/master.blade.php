73096b93ef235c2860546205a99eeb3480f33196
stanley jony <1jony2jony@gmail.com>
2023-02-28 15:15:21 -0300

    Entendendo o uso de helpers

--- project_web/web_site4/resources/views/front/master/master.blade.php
+++ project_web/web_site4/resources/views/front/master/master.blade.php
@@ -1,45 +1,41 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Learn &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="author" content="freehtml5.co" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	{{-- <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet"> --}}
-    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
-    <link href="https://fonts.googleapis.com/css2?family=Miriam+Libre:wght@400;700&display=swap" rel="stylesheet">
+    {!! $head ?? '' !!}
+    <meta name="author" content="freehtml5.co"/>
 
-    <link rel="stylesheet" href="{{ url(mix('front/assets/css/vendor.css')) }}">
-    <link rel="stylesheet" href="{{ url(mix('front/assets/css/style.css')) }}">
+    <link href="https://fonts.googleapis.com/css?family=Miriam+Libre:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix_e('front/assets/css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ mix_e('front/assets/css/style.css') }}">
+    @yield('stylesheet')
 
-	<!-- Modernizr JS -->
-	<script src="{{ url(mix('front/assets/js/modernizr.js')) }}"></script>
+    <script src="{{ mix_e('front/assets/js/modernizr.js') }}"></script>
 
-	</head>
-	<body>
+</head>
+<body>
 
-	<div class="fh5co-loader"></div>
+<div class="fh5co-loader"></div>
 
-	<div id="page">
+<div id="page">
 
-        @include('front.includes.header')
+    @include('front.includes.header')
 
-        @yield('content')
+    @yield('content')
 
-        @include('front.includes.footer')
+    @include('front.includes.footer')
+</div>
 
-	</div>
+<div class="gototop js-top">
+    <a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+</div>
 
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
-	</div>
+<script src="{{ mix_e('front/assets/js/vendor.js') }}"></script>
+<script src="{{ mix_e('front/assets/js/main.js') }}"></script>
+@yield('scripts')
 
-	<script src="{{ url(mix('front/assets/js/vendor.js')) }}"></script>
-	<script src="{{ url(mix('front/assets/js/main.js')) }}"></script>
-
-	</body>
+</body>
 </html>
-
